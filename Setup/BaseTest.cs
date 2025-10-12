f7fb502e23bf32cf4e2bc63706d00be7dd960061
=Rahul Kelkar <RahulKelkar44@gmail.com>
2025-10-13 02:07:29 +0530

    Fix : TestHooks
    Driver should instansiate amd crete in each test run not onetime

--- Setup/BaseTest.cs
+++ Setup/BaseTest.cs
@@ -17,21 +17,21 @@ namespace SauceAppTests.Setup
 		{
 			logger.Information("Starting the test run onetime setup");
 			LoggerConfig.Initialize();
-			Driver = DriverFactory.CreateDriver();
 		}
 		[SetUp]
 		public void Setup()
 		{
 			logger.Information("Starting the test run setup");
 			string baseUrl = GlobalVariable.BaseUrl ?? throw new InvalidOperationException("Base URL is not set in GlobalVariable.");
+			Driver = DriverFactory.CreateDriver();
 			Driver!.Navigate().GoToUrl(baseUrl);// Navigate to the login page
 		}
 		[TearDown]
 		public void TearnDown()
 		{
+			Driver!.Quit();
 			logger.Information("Starting the test run teardown");
 			logger.Information("Closing the browser and cleaning up resources");
-			Driver!.Quit();
 		}
 		[OneTimeTearDown]
 		public void OneTimeTearDown()
