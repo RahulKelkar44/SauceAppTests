64e1912f3b4645940c2acdc5d70658559e3f8d16
=Rahul Kelkar <RahulKelkar44@gmail.com>
2025-10-13 02:06:17 +0530

    Add Inventory Page
    Add Initial things to invrntory,html page class ,
    We have started with PageTitle

---
+++ PageObjects/InventoryPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using Serilog;
+
+namespace SauceAppTests.PageObjects
+{
+	internal class InventoryPage
+	{
+		//WebDriver 
+		private readonly IWebDriver _driver;
+
+		//Logger
+		private readonly ILogger _logger = Log.ForContext<InventoryPage>();
+
+		//WebElement Fields
+		private Lazy<IWebElement> pageTitle;
+
+		//Corresponding Property
+		public Lazy<IWebElement> PageTitle  => pageTitle;
+
+		// Id Locators for fiding WebElements
+		private const string pageTitleClassName = "app_logo";
+
+		public InventoryPage(IWebDriver driver)
+		{
+			_driver = driver;
+			_logger.Information("Initializing WebElements in Inventorry Page");
+			pageTitle = new Lazy<IWebElement>(() => _driver.FindElement(By.ClassName(pageTitleClassName)));
+			_logger.Information("Initialization complete in Inventorry Page");
+
+		}
+
+
+	}
+}
