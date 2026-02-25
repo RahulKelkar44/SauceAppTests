99524ffca9ab865831888fdba7e040ca5343acdb
=Rahul Kelkar <RahulKelkar44@gmail.com>
2026-02-25 22:34:48 +0530

    Add headless automation options for CI

--- Setup/DriverFactory.cs
+++ Setup/DriverFactory.cs
@@ -17,6 +17,10 @@ namespace SauceAppTests.Setup
 			options.AddUserProfilePreference("credentials_enable_service", false);
 			options.AddUserProfilePreference("profile.password_manager_enabled", false);
 			options.AddUserProfilePreference("profile.password_manager_leak_detection", false);
+			options.AddArgument("--headless=new");
+			options.AddArgument("--no-sandbox");
+			options.AddArgument("--disable-dev-shm-usage");
+			options.AddArgument("--disable-gpu");
 			return new ChromeDriver(options);
 		}
 	}
