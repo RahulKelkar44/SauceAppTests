d6b009aa51fc329a6875f5c074354389096a2286
=Rahul Kelkar <RahulKelkar44@gmail.com>
2026-03-09 23:12:45 +0530

    Working Directory

--- Setup/GlobalVariable.cs
+++ Setup/GlobalVariable.cs
@@ -1,12 +1,14 @@
 ﻿
 
+using NUnit.Framework;
+
 namespace SauceAppTests.Setup
 {
 	internal static  class GlobalVariable
 	{
 		internal static string? BaseUrl { get; set; }
 
-		internal static string BaseDirectory = Directory.GetCurrentDirectory();
+		internal static string BaseDirectory = TestContext.CurrentContext.TestDirectory;
 		internal static string? TestResultPath { get; set; }
 
 	}
